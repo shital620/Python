713a0e713b106a218e8d63091de029c9ade94b57
shital620 <161359973+shital620@users.noreply.github.com>
2025-02-02 08:46:34 +0530

    Delete python/operators/conditional statement directory

--- python/operators/conditional statement/clever if.py
+++
@@ -1,7 +0,0 @@
-age=int(input("age :"))
-vote=("no","yes")[age>=18]
-print(vote)
-
-# sal=float(input("salary :"))
-# tax=sal*(o.1,o.2)[sal>5000]
-# print(tax)
--- python/operators/conditional statement/ternary operation.py
+++
@@ -1,3 +0,0 @@
-food=input("food name :")
-eat="yes"if food=="cake" else "no"
-print(eat)
